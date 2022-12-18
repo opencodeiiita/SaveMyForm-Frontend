eb83fc46caaceaa51715676ea8630ad2d685f828
Shubhankit-Pandey <shubhankitpandey15@gmail.com>
2022-12-18 23:44:28 +0530

    Requested Changes Done

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -5,27 +5,27 @@ import { Typography } from "antd"
 export default function SignUp() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
-            <div className="bg-[#FFFEEF] hover:bg-[#FFFFFF]">
+            <div className="bg-gray-200">
                 
             </div>
-            <div className="bg-[#00694B] flex flex-col justify-center font-sans">
-                <form className="max-w-[500px] w-full mx-auto rounded-lg bg-[#0c5e46f6] p-10 ">
-                    <h2 className="text-4xl text-center text- p-5 text-[#FFFEFE] font-bold">
-                        SIGN UP
+            <div className=" flex flex-col justify-center font-sans">
+                <form className="max-w-[500px] w-full mx-auto rounded-lg p-10 ">
+                    <h2 className="text-4xl text-center text- p-5 text-[#00694B] font-bold">
+                        Sign up
                     </h2>
-                    <div className="flex flex-col py-2 text-[#FFFEFE] ">
-                        <input placeholder="Name" className="font-normal rounded-lg bg-[#00694B] p-3" type="text"></input>
+                    <div className="flex flex-col py-2 text-[#00694B] ">
+                        <input placeholder="Name" className="border-2 font-normal rounded-lg bg-[#FFFFFF] p-3 hover:border-blue-500" type="text"></input>
                     </div>
-                    <div className="flex flex-col py-2 text-[#FFFEFE] ">
-                        <input placeholder="Email" className="font-normal rounded-lg bg-[#00694B] p-3"  type="text"></input>
+                    <div className="flex flex-col py-2 text-[#00694B] ">
+                        <input placeholder="Email" className="border-2 font-normal rounded-lg bg-[#FFFFFF] p-3 hover:border-blue-500"  type="text"></input>
                     </div>
-                    <div className="flex flex-col py-2 text-[#FFFEFE] ">
-                        <input placeholder="Password" className="font-normal rounded-lg bg-[#00694B] p-3"  type="text"></input>
+                    <div className="flex flex-col py-2 text-[#00694B] ">
+                        <input placeholder="Password" className="border-2 font-normal rounded-lg bg-[#FFFFFF] p-3 hover:border-blue-500"  type="text"></input>
                     </div>
-                    <div className="flex flex-col py-2 text-[#FFFEFE] ">
-                        <input placeholder="Confirm Password" className="font-normal rounded-lg bg-[#00694B] p-3"  type="text"></input>
+                    <div className="flex flex-col py-2 text-[#00694B] ">
+                        <input placeholder="Confirm Password" className="border-2 font-normal rounded-lg bg-[#FFFFFF] p-3 hover:border-blue-500"  type="text"></input>
                     </div>
-                    <button className="w-full my-2 py-3 bg-[#FFFFFF] shadow-lg shadow-white-500/50 rounded-lg text-[#00694B] hover:bg-[#FFFEEE]">Submit</button>
+                    <button className="border-2 border-[#00694B] border-opacity-70 w-full my-2 py-3 bg-[#00694B] shadow-lg shadow-black-500/50 rounded-lg text-[#FFFFFF] hover:border-[#12AB52]">Submit</button>
                 </form>
             </div>
         </div>
