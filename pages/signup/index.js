b0355c5ff242b52ec90f7d2b2621e6ce977130ac
Shubhankit-Pandey <shubhankitpandey15@gmail.com>
2022-12-18 17:32:58 +0530

    changes

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,1 +1,12 @@
-export default function SignUp() {}
+import React from "react"
+import { Typography } from "antd"
+import ""
+
+export default function SignUp() {
+    return (
+        <div className="left-0">
+            <h1>ssad</h1>
+        </div>
+          
+      );
+}
