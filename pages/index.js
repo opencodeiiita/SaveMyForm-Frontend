9e31f24718eb377845bd35508e28a0df524ee3bd
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-12-25 18:33:56 +0530

    changes on signup page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Dashboard from "./dashboard";
-import FAQ from "./faq";
-import Alert from "../components/elements/Alert"
 
 export default function Index() {
-    return <>
-        <Alert />
-        <Dashboard />
-    </>;
+    return <></>;
 }
