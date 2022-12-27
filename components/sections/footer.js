53d09352686cf2cae6a43171a9e195106e580543
Shubhankit-Pandey <shubhankitpandey15@gmail.com>
2022-12-27 19:47:56 +0530

    minor changes

--- components/sections/footer.js
+++ components/sections/footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Image from "next/image"
-import down from "../../assets/images/illustrations/down.png"
+import down from "../../assets/images/illustrations/Down.png"
 
 export default function Footer(){
     return(
