94762b8dee384a142ae59b18a39f950280a10859
Gamma Microwave <pranavt150@gmail.com>
2022-12-28 13:13:43 +0530

    made the 1000 responses page (#87)
    * added a new section in homepage
    
    * made few chaanges in responsiveness
    
    * jo
    
    * fixed bugs regarding responsiveness
    
    * small line change

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,16 @@
 import HeroSection from "../../components/elements/HeroSection";
 import Feature from "../../components/sections/feature.js";
 import Footer from "../../components/sections/footer";
-
+import Responses from "../../components/sections/responses.js";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Feature />
-      <Footer/>
+      {/* <div className="h-[100vh] w-[100vw] bg-red-500"></div> */}
+      <Responses />
+      <Footer />
     </>
   );
 }
