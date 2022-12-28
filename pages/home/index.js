c8dd5b8d369cbeaa27a07e04593ca725ad243559
Vishal Ola <vishalola@Vishals-MacBook-Air.local>
2022-12-28 20:12:39 +0530

    responsive

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,12 +1,13 @@
 import HeroSection from "../../components/elements/HeroSection";
 import Feature from "../../components/sections/feature.js";
-
+import FormResponse from "../../components/sections/FormResponsesSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Feature />
+      <FormResponse/>
     </>
   );
 }
