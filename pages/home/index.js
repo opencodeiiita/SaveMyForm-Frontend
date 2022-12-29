deb9f7ecb9677b65329ba9e6e9a0f61875282bed
Vishal Ola <98338637+vishalola@users.noreply.github.com>
2022-12-30 02:26:02 +0530

    ReCaptcha Section added (#93)
    Co-authored-by: Vishal Ola <vishalola@Vishals-MacBook-Air.local>

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,7 +3,7 @@ import Feature from "../../components/sections/feature.js";
 import Footer from "../../components/sections/footer";
 import Responses from "../../components/sections/responses.js";
 import DownloadFormResponses from "../../components/sections/FormResponsesSection";
-
+import ReCaptcha from "../../components/sections/reCaptcha";
 export default function Home() {
   return (
     <>
@@ -12,6 +12,7 @@ export default function Home() {
       {/* <div className="h-[100vh] w-[100vw] bg-red-500"></div> */}
       <DownloadFormResponses/>
       <Responses />
+      <ReCaptcha/>
       <Footer />
     </>
   );
