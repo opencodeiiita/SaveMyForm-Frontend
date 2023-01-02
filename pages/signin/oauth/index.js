446c9091c910f164513432a21a117ada0a6ac66a
guptamanas149@gmail.com <guptamanas149@gmail.com>
2023-01-02 21:15:35 +0530

    udpated oauth

--- pages/signin/oauth/index.js
+++ pages/signin/oauth/index.js
@@ -1,35 +1,33 @@
 import { useRouter } from "next/router";
-import {post} from "../../../components/utils/API"
+import { post } from "../../../components/utils/API";
 import { useEffect } from "react";
 
 export default function OAuth() {
-    
     const router = useRouter();
 
-    const  auth = async() => {
-    let code = router.query.code;
-    console.log(code);
-    const response = await post("/auth/google", {
-        token : code,
-    },);
+    const auth = async () => {
+        let code = router.query.code;
+        console.log(code);
+        const response = await post("/auth/google", {
+            token: code,
+        });
 
-    console.log(response);
-    }
+        console.log(response);
+        if (response.status === 201) router.replace("/dashboard");
+    };
 
     useEffect(() => {
-        if(router.isReady){
+        if (router.isReady) {
             auth();
         }
-    },[router]);
+    }, [router]);
 
     return (
-        <div class="grid h-screen place-content-center">
-            <div class="flex items-center gap-2">
-                <span class="h-10 w-10 block rounded-full border-4 border-t-blue-700 animate-spin"></span>
+        <div className="grid h-screen place-content-center">
+            <div className="flex items-center gap-2">
+                <span className="h-10 w-10 block rounded-full border-4 border-t-blue-700 animate-spin"></span>
                 Loading...
             </div>
         </div>
-
-    )
-
+    );
 }
