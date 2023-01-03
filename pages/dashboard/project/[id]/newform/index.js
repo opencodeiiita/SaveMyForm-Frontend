4daeb069bbdc79acc9c16bb547abe37cc124e257
guptamanas149@gmail.com <guptamanas149@gmail.com>
2023-01-03 16:08:38 +0530

    fixed new form page

--- pages/dashboard/project/[id]/newform/index.js
+++ pages/dashboard/project/[id]/newform/index.js
@@ -1,31 +1,91 @@
-import { Input, Checkbox, Label } from "antd";
+import { useState } from "react";
+import {
+    Button,
+    Form,
+    Input,
+    Typography,
+    message,
+    Col,
+    Checkbox,
+    Label,
+} from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 export default function NewForm() {
-  return (
-    <>
-      <div className=" mt-4">
-        <div className="mx-2 rounded-lg shadow-[0_4px_4px_rgba(0,0,0,0.25)] h-fit w-1/1 p-5">
-          <div className="flex flex-col">
-            <label htmlFor="project_name" className="mx-2">
-              Project Name
-            </label>
-            <Input
-              className="inline-block w-[100%] border-2 border-r-[#FFFEFE] border-t-[#FFFEFE] border-l-[#FFFEFE] border-b-[#C2C8CB] font-normal text-xs rounded-md mt-2 bg-[#FFFEFE]"
-              type="text"
-              placeholder="Project Name"
-              required
-            />
-          </div>
+    const [isChecked, setIsChecked] = useState(false);
+    const [projectName, setProjectName] = useState("");
+    const [reCaptchaKey, setReCaptchaKey] = useState("");
+    const [reCaptchaSecret, setReCaptchaSecret] = useState("");
 
-          <div className="flex flex-col mt-5">
-            <label htmlFor="captcha" className="">
-              reCAPTCHA v3
-            </label>
-            <Checkbox className="w-[60%] mt-2">
-              Do You Want reCaptcha Support
-            </Checkbox>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    const handleChange = (e) => {
+        setIsChecked(e.target.checked);
+    };
+    const clearInputs = () => {
+        setProjectName("");
+        setReCaptchaKey("");
+        setReCaptchaSecret("");
+        setIsChecked(false);
+    };
+    return (
+        <>
+            <form className=" bg-[#FFFEFE] max-w-4xl mx-8">
+                <Typography.Title
+                    level={3}
+                    className="mt-10 mb-0 text-base font-inter font-bold text-left"
+                >
+                    Form Name:{" "}
+                    <ExclamationCircleOutlined className="text-lg text-[#7f7f7f] pl-4" />
+                    <Input
+                        placeholder="e.g example.com"
+                        className=" border-2 border-r-[#FFFEFE] border-t-[#FFFEFE] border-l-[#FFFEFE] border-b-[#C2C8CB] font-normal text-normal rounded-md bg-[#FFFEFE] p-3 focus:valid:border-b-green-500 focus:invalid:border-b-red-500 transition-all"
+                        type="text"
+                        value={projectName}
+                        onChange={(e) => setProjectName(e.target.value)}
+                        required
+                    />
+                </Typography.Title>
+                <Typography.Title
+                    level={3}
+                    className="mt-10 mb-0 text-base font-inter font-bold text-left"
+                >
+                    reCAPTCHA v3:{" "}
+                    <ExclamationCircleOutlined className="text-lg text-[#7f7f7f] pl-4" />
+                </Typography.Title>
+                <Checkbox onChange={handleChange} checked={isChecked}>
+                    Do you want reCaptcha Support?
+                </Checkbox>
+                {isChecked && (
+                    <>
+                        <Input
+                            placeholder="Enter reCaptcha Key"
+                            className="mb-4 border-2 border-r-[#FFFEFE] border-t-[#FFFEFE] border-l-[#FFFEFE] border-b-[#C2C8CB] font-normal text-normal rounded-md bg-[#FFFEFE] p-3 focus:valid:border-b-green-500 focus:invalid:border-b-red-500 transition-all"
+                            value={reCaptchaKey}
+                            onChange={(e) => setReCaptchaKey(e.target.value)}
+                            required
+                        />
+                        <Input
+                            placeholder="Enter reCaptcha Secret"
+                            className=" border-2 border-r-[#FFFEFE] border-t-[#FFFEFE] border-l-[#FFFEFE] border-b-[#C2C8CB] font-normal text-normal rounded-md bg-[#FFFEFE] p-3 focus:valid:border-b-green-500 focus:invalid:border-b-red-500 transition-all"
+                            value={reCaptchaSecret}
+                            onChange={(e) => setReCaptchaSecret(e.target.value)}
+                            required
+                        />
+                    </>
+                )}
+                <Button
+                    type="secondary"
+                    className=" border-[#00694B] text-[#00694B] font-medium font-inter  my-4 pb-2 rounded-md "
+                    onClick={clearInputs}
+                >
+                    Cancel
+                </Button>
+                <Button
+                    type="primary"
+                    className=" ml-4 border-[#00694B] text-[#FFFEFE] font-medium font-inter rounded-md my-4 pb-2 "
+                    // onClick={handleSubmit}
+                >
+                    Submit
+                </Button>
+            </form>
+        </>
+    );
 }
