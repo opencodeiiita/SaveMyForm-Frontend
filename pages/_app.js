f4e237c325547e26b633c6ef63dd72bb235dcc85
Gamma Microwave <pranavt150@gmail.com>
2023-01-04 01:50:25 +0530

    added reCaptcha functionality in sign up page (#121)
    * added newform component
    
    * added reCaptcha functionality in signup page

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,25 @@ import "../styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import Appbar from "../components/elements/Appbar/index";
 import Alert from "../components/elements/Alert";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 function MyApp({ Component, pageProps }) {
-    return (
-        <NextUIProvider>
-            {/* <Alert /> */}
-            <Appbar />
-            <Component {...pageProps} />
-        </NextUIProvider>
-    );
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={"6LcZo3cjAAAAAPEshUhFpjSOLdDaTQEbSoEwwB67"}
+      scriptProps={{
+        async: false,
+        defer: true,
+        appendTo: "body",
+        nonce: undefined,
+      }}
+    >
+      <NextUIProvider>
+        {/* <Alert /> */}
+        <Appbar />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </GoogleReCaptchaProvider>
+  );
 }
 
 export default MyApp;
