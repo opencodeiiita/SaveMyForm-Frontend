472a49940ff7a53aadfda316a8d7427f4a3678cf
TheDivineAurora <wolfenbitzyt@gmail.com>
2023-01-06 22:56:12 +0530

    missing code

--- components/sections/FormResponsesSection.js
+++ components/sections/FormResponsesSection.js
@@ -35,13 +35,13 @@ export default ()=>{
 </defs>
 </svg>
           
-            <Image ref={circle1} src={Ellips} className={`figma_ellipse max-md:w-[150px] max-md:h-[150px] max-md:mt-[22vh] max-lg:mt-[16vh]  max-2xl:ml-[10vw] 2xl:ml-[20vw] reveal-bottom ${
+            <Image ref={circle1} src={Ellips} className={`figma_ellipse max-md:w-[150px] max-md:h-[150px] 2xl:w-[15vw] 2xl:h-[15vw] max-md:mt-[22vh] max-lg:mt-[16vh] 2xl:mt-[-5vw] max-xl:ml-[15vw] max-2xl:ml-[5vw] 2xl:ml-[10vw] reveal-bottom ${
             circle1Visible ? "active-reveal" : ""
           }`}/>
             <Image ref={circle2} src={Ellips} className={`figma_ellipse2 max-lg:mt-[36vh] max-xl:mt-[22vh] max-2xl:mt-[10vh] reveal-bottom ${
             circle2Visible ? "active-reveal" : ""
           }`}/> 
-            <Image ref={vectorImg} src={Vector} className={`figma_vector min-[1440px]:w-[20vw] mt-[-10%] reveal-bottom ${
+            <Image ref={vectorImg} src={Vector} className={`figma_vector xl:w-[40vw] mt-[-10%] reveal-bottom ${
             vectorImgVisible ? "active-reveal" : ""
           }`}/>
             
